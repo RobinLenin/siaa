ef3d742e655fd9ad040654f566356617794cb5a7
Robin Cordova <robincordova7@gmail.com>
2019-09-20 10:56:18 -0500

    fragmento_lista intereses mensuales

--- app/tesoreria/admin.py
+++ app/tesoreria/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import CuentaCobrar, Abono, TasaInteres, Comentario
+from .models import CuentaCobrar, Abono, TasaInteres, Comentario, InteresMensual
 
 from app.seguridad.admin import AuditModelAdmin
 
@@ -30,8 +30,15 @@ class TasaInteresAdmin(admin.ModelAdmin):
     list_display = ('anio', 'mes', 'tasa',)
     #raw_id_fields = ('cliente',)
 
+class InteresMensualAdmin(admin.ModelAdmin):
+    # fields = ('codigo', 'descripcion', 'definicion', 'updated_by', 'created_by', 'created_at', 'updated_at',)
+    # search_fields = ('descripcion', 'codigo',)
+    list_display = ('fecha', 'valor', 'tasa',)
+    # raw_id_fields = ('cliente',)
+
 admin.site.register(CuentaCobrar, CuentaCobrarAdmin)
 admin.site.register(Abono, AbonoAdmin)
 admin.site.register(Comentario, ComentarioAdmin)
 admin.site.register(TasaInteres, TasaInteresAdmin)
+admin.site.register(InteresMensual, InteresMensualAdmin)
 
